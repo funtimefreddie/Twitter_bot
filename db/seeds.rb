92f420026f7b0858effc464882e4014c828d82ea
Freddie Waite <f.m.waite@gmail.com>
2015-03-02 08:49:19 +0800

    seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,13 @@
 
 
 Flirt.create!([
-  {message: "*blushing* you're too cute", opening_line: false, suggested: true, sent_before: false},
- {message: "I think you're suffering from a lack on vitamin me.", opening_line: false, suggested: true, sent_before: false},
-{message: "I'm so happy this is finally happening", opening_line: false, suggested: true, sent_before: false},
-{message: "Wanna get some coffee, Because I like you a Latte", opening_line: true, suggested: true, sent_before: false},
-{message: "If kisses were snowflakes, I'd send you a blizzard.", opening_line: true, suggested: true, sent_before: false},
-{message: "Can I tie your shoe? Because I can't have you fall for anyone else", opening_line: true, suggested: true, sent_before: false},
+  
+{message: "We would be so great together", opening_line: false, suggested: true, sent_before: false},
+{message: "Are you ready yet to meet my parents?", opening_line: false, suggested: true, sent_before: false},
+{message: "I really like this heat between us", opening_line: false, suggested: true, sent_before: false},
+{message: "Hey man, I think you dropped your gay card", opening_line: true, suggested: true, sent_before: false},
+{message: "Did I leave my hot pants at your place?", opening_line: true, suggested: true, sent_before: false},
+{message: "Hey homebre, want to sit on my lap and talk about this first thing that comes up?", opening_line: true, suggested: true, sent_before: false},
+
   
 ])
