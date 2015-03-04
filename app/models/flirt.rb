625baadbb64ccc0403535fe8fb09f33cde115777
Freddie Waite <f.m.waite@gmail.com>
2015-03-04 11:01:58 +0800

    added action to review

--- app/models/flirt.rb
+++ app/models/flirt.rb
@@ -1,2 +1,14 @@
 class Flirt < ActiveRecord::Base
+
+  def self.review
+
+    Flirt.where(sent_before: false).each { |f|
+      puts f.message
+      
+      if gets.chomp == "D"
+        f.delete
+      end
+    }
+
+  end
 end
