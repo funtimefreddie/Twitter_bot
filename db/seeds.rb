fd0976f18e15a3939ecdb2225159a3367c8005e8
Freddie Waite <f.m.waite@gmail.com>
2015-03-04 10:51:09 +0800

    more seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,8 @@
-
-
 Flirt.create!([
-  
-{message: "We would be so great together", opening_line: false, suggested: true, sent_before: false},
-{message: "Are you ready yet to meet my parents?", opening_line: false, suggested: true, sent_before: false},
-{message: "I really like this heat between us", opening_line: false, suggested: true, sent_before: false},
-{message: "Hey man, I think you dropped your gay card", opening_line: true, suggested: true, sent_before: false},
-{message: "Did I leave my hot pants at your place?", opening_line: true, suggested: true, sent_before: false},
-{message: "Hey homebre, want to sit on my lap and talk about this first thing that comes up?", opening_line: true, suggested: true, sent_before: false},
-
-  
+  {message: "Spank you very much! :D", opening_line: false, suggested: false, tweet_id: nil, sent_before: false},
+  {message: "It's not your fault...it's not your fault...it's not your fault...it's not your fault...it's not your fault...it's not your fault...", opening_line: true, suggested: false, tweet_id: nil, sent_before: false},
+  {message: "I can't think straight when you talk to me like this *drools*", opening_line: false, suggested: false, tweet_id: nil, sent_before: false},
+  {message: "Nice butt! What time does it open?", opening_line: true, suggested: false, tweet_id: nil, sent_before: false},
+  {message: "Bad attitude - check.  Bad grammar - check.  Bulge in my pants - check!", opening_line: true, suggested: false, tweet_id: nil, sent_before: false},
+  {message: "Your obsession with my lifestyle is so endearing.  Thank you, for caring", opening_line: true, suggested: false, tweet_id: nil, sent_before: false},
 ])
