2215dbd0e2f18c4f7f2086f877441f1f72f04b5b
Freddie Waite <f.m.waite@gmail.com>
2015-03-05 11:44:53 +0800

    one more seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,5 @@
 Flirt.create!([
-  {message: "Want to join the gay mafia?  We're recruiting.", opening_line: true, suggested: false, tweet_id: nil, sent_before: false},
-  
-  {message: "How much do you bench? Bet it's loads and loads...and loads...and loads...and...what were we talking about again?", opening_line: false, suggested: false, tweet_id: nil, sent_before: false},
- 
-  {message: "Where is your favourite place to be tickled?  Mine is your bed.", opening_line: true, suggested: false, tweet_id: nil, sent_before: false},
-  {message: "Want to come to my place and make a quiche?", opening_line: true, suggested: false, tweet_id: nil, sent_before: false},
+  {message: "Asides from being sexy...what do you do for a living?", opening_line: true, suggested: false, tweet_id: nil, sent_before: false},
   
   
-  {message: "Where does this anger come from?  I just can't get enough.", opening_line: false, suggested: false, tweet_id: nil, sent_before: false},
-  {message: "I want to fold you like origami", opening_line: false, suggested: false, tweet_id: nil, sent_before: false},
-  {message: "This is so us", opening_line: false, suggested: false, tweet_id: nil, sent_before: false},
 ])
