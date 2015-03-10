cc48d608e87ac79cb76457d6e3be2a0cce0016c5
Freddie Waite <f.m.waite@gmail.com>
2015-03-10 11:13:57 +0800

    more seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,7 @@
 Flirt.create!([
-  {message: "Are you tired?  Because you've been running through my pants all day", opening_line: true, suggested: false, tweet_id: nil, sent_before: false},
-  {message: "How do you like your eggs in the morning?  I like mine with a side of you - scrumptious!!", opening_line: true, suggested: false, tweet_id: nil, sent_before: false},
-  {message: "Your sculpted bod has inspired me to work out.  So far I have worked out that we would make a crazy cute couple", opening_line: true, suggested: false, tweet_id: nil, sent_before: false},
-  {message: "Touché, homebre ;)", opening_line: false, suggested: false, tweet_id: nil, sent_before: false},
-  {message: "You'll find your dream faggot one day, I promise you.  Just keep on trucking kiddo *ruffles hair*.", opening_line: true, suggested: false, tweet_id: nil, sent_before: false},
+  
+  {message: "It's not your fault...it's not your fault...it's not your fault...it's not your fault... http://www.joblo.com/newsimages1/robinwilliams-3.jpg", opening_line: false, suggested: false, tweet_id: nil, sent_before: false},
+  
   
  
 ])
